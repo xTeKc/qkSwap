2c4cf16092c9ff9975daa60658866548e09f2684
xTeKc <bcd3v@pm.me>
2021-07-05 20:05:48 -0400

    update network

--- truffle-config.js
+++ truffle-config.js
@@ -2,23 +2,35 @@ require('babel-register');
 require('babel-polyfill');
 
 module.exports = {
+
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*" //match any network id
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), //Array of account private keys
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}` //Url to ETH node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
     },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
+
+  // Configure your compilers
   compilers: {
     solc: {
       version: "0.8.0",
       optimizer: {
         enabled: true,
         runs: 200
-      },
-      evmVersion: "petersburg"
+      }
     }
-  }
-}
+  },
