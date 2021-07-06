24743f943090b3abc0b354cec6819804795a1b64
xTeKc <bcd3v@pm.me>
2021-07-06 19:30:40 -0400

    fetch deployed token

--- scripts/seed-QkSwap.js
+++ scripts/seed-QkSwap.js
@@ -15,5 +15,9 @@ module.exports = async (callback) {
     try {
         //Fetch accounts from wallet (they are unlocked)
         const accounts = await web3.eth.getAccounts()
+
+        //Fetch the deployed Token
+        const token = await Token.deployed()
+        console.log('Token fetched', token.address)
     }
 }  
