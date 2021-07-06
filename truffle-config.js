97c5c511d02ec41cb2ff82b180a4699d193a96ea
xTeKc <bcd3v@pm.me>
2021-07-05 20:17:03 -0400

    update

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,17 @@
 require('babel-register');
 require('babel-polyfill');
+require("dotenv").config();
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 module.exports = {
 
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" //match any network id
-    },
+    // development: {
+    //   host: "127.0.0.1",
+    //   port: 7545,
+    //   network_id: "*" //match any network id
+    // },
     kovan: {
       provider: function() {
         return new HDWalletProvider(
