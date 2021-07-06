f3ea273e8ad53019fcc0d08e16efa4db3e570540
xTeKc <bcd3v@pm.me>
2021-07-06 19:20:41 -0400

    add ether utils

--- scripts/seed-QkSwap.js
+++ scripts/seed-QkSwap.js
@@ -2,3 +2,11 @@
 const Token = artifacts.require('Token');
 const QkSwap = artifacts.require('QkSwap');
 
+//Utils
+const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000' //Ether token deposit address
+
+const ether = (n) => {
+  return new web3.utils.BN(
+    web3.utils.toWei(n.toString(), 'ether')
+  )
+}
